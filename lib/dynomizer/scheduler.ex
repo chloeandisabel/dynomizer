fd979af09a035b31eada4aa38dd51900d6718a64
Jim Menard <jim.menard@chloeandisabel.com>
2017-01-07 11:44:16 -0500

    Scheduler rework.

--- lib/dynomizer/scheduler.ex
+++ lib/dynomizer/scheduler.ex
@@ -1,4 +1,24 @@
 defmodule Dynomizer.Scheduler do
+  @moduledoc """
+
+  Loads Dynomizer.Schedule instances and schedules each for execution based
+  on its schedule method (cron or at). Uses Quantum
+  (https://github.com/c-rack/quantum-elixir) for cron-style scheduling and
+  `Process.send_after` for at-style scheduling.
+
+ `refresh` loads all schedules, cancels all modified jobs (as determined by
+  the updated_at datetime), and schedules all new and changed schedules. We
+  use Quantum to schedule execution of `refresh` at regular one-minute
+  intervals.
+
+ `run_at` handles execution of a single job.
+
+  The state contains a map whose keys are schedule ids and values are tuples
+  of the form `{schedule, arg}`. `arg` is whatever term can be used to
+  identify and stop the job: either a Quantum job name or a `Process` timer,
+  depending on the schedule's method.
+  """
+
   use GenServer
   require Logger
   alias Dynomizer.{Repo, Schedule, Heroku}
@@ -19,72 +39,100 @@ defmodule Dynomizer.Scheduler do
 
   # ================ handlers ================
 
-  def handle_call(:refresh, _from, state) do
+  def handle_call(:refresh, _from, running) do
     Logger.debug "Dynomizer.Scheduler#refreshing"
-    stop_all_jobs(state)
-    new_state =
-      Schedule
-      |> Repo.all
-      |> start_all_jobs
-    {:reply, :ok, new_state}
+    {:reply, :ok, reschedule(running)}
   end
 
-  def handle_call({:run_at, app, dyno_type, rule}, _from, state) do
+  def handle_call({:run_at, app, dyno_type, rule}, _from, running) do
     Heroku.scale(app, dyno_type, rule)
-    {:reply, :ok, state}
+    {:reply, :ok, running}
   end
 
-  def terminate(reason, state) do
-    stop_all_jobs(state)
+  def terminate(reason, running) do
+    stop_jobs(running)
     if reason != :shutdown, do: Logger.info("Dynomizer.Scheduler.terminate: #{inspect reason}")
   end
 
   # ================ helpers ================
 
-  defp start_all_jobs(schedules) do
+  # Loads schedules and sets their method virtual fields.
+  defp load_schedules do
+    Schedule
+    |> Repo.all
+    |> Enum.map(fn s -> %{s | method: Schedule.method(s)} end)
+  end
+
+  # Stops deleted jobs, starts new jobs, and restarts modified ones. Returns
+  # a map of the new set of running jobs.
+  defp reschedule(running) do
+    old_schedules = running |> Map.values |> Enum.map(fn {s, _} -> s end)
+    new_schedules = load_schedules()
+    {new, mod, del, unch} = Schedule.partition(old_schedules, new_schedules)
+    reschedule(new, mod, del, unch, running)
+  end
+
+  # Short-circuit the most common case: nothing has changed.
+  defp reschedule([], [], [], _, running), do: running
+  defp reschedule(new, mod, del, unch, running) do
+    # Stop deleted and modified jobs
+    running
+    |> Map.take(ids(del ++ mod))
+    |> stop_jobs
+
+    # Start new and modified jobs
+    started_map = start_jobs(new ++ mod)
+
+    running
+    |> Map.take(ids(unch))
+    |> Map.merge(started_map)
+  end
+
+  # Start each scheduled job and return a state map.
+  defp start_jobs(schedules) do
     schedules
-    |> Enum.map(&start_job/1)
+    |> Enum.map(&({&1.id, {&1, start_job(&1)}}))
+    |> Enum.into(%{})
   end
 
-  defp stop_all_jobs(state) do
-    state
-    |> Enum.each(&stop_job/1)
-  end
-
-  # Dispatch to start_job/3 based on schedule method.
-  defp start_job(s) do
-    start_job(s, Schedule.method(s), job_name(s))
-  end
-
-  defp start_job(s, :cron, name) do
+  # Starts a job and returns whatever term can be used to identify and stop
+  # the job.
+  defp start_job(%Schedule{method: :cron} = s) do
+    name = job_name(s)
     job = {s.schedule, &run_at/3, [s.application, s.dyno_type, s.rule]}
     Quantum.add_job(name, job)
-    {name, :cron, nil}
+    name
   end
-  defp start_job(s, :at, name) do
+  defp start_job(%Schedule{method: :at} = s) do
     at = Schedule.to_unix_milliseconds(s)
     now = DateTime.utc_now |> DateTime.to_unix(:milliseconds)
     if at >= now do
       msg = {:run_at, s.application, s.dyno_type, s.rule}
-      timer = Process.send_after(__MODULE__, msg, at, abs: true)
-      {name, :at, timer}
+      Process.send_after(__MODULE__, msg, at, abs: true)
     else
-      {name, :at, nil}
+      nil
     end
   end
 
-  defp stop_job({name, :cron, _}) do
+  # Take a map (state or a subset of it) and stops all of those jobs.
+  defp stop_jobs(running) do
+    running
+    |> Map.values
+    |> Enum.each(fn {s, arg} -> stop_job(s.method, arg) end)
+  end
+
+  defp stop_job(:cron, name) do
     Quantum.delete_job(name)
   end
-  defp stop_job({_, :at, nil}) do
+  defp stop_job(:at, nil) do
     # nop
   end
-  defp stop_job({_, :at, timer}) do
+  defp stop_job(:at, timer) do
     # It's OK if the timer has already been sent.
     Process.cancel_timer(timer)
   end
 
-  defp job_name(%Schedule{id: id}) do
-    "job#{id}"
-  end
+  defp job_name(%Schedule{id: id}), do: "job#{id}"
+
+  defp ids(schedules), do: schedules |> Enum.map(&(&1.id))
 end
