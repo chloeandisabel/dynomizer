53ba239c7a875317c9f640ea464cfb12c3ba7767
Jim Menard <jim.menard@chloeandisabel.com>
2017-01-07 11:32:36 -0500

    Schedule.partition and method virtual field

--- test/models/schedule_test.exs
+++ test/models/schedule_test.exs
@@ -1,5 +1,6 @@
 defmodule Dynomizer.ScheduleTest do
   use Dynomizer.ModelCase
+  doctest Dynomizer.Schedule
 
   alias Dynomizer.Schedule
 
@@ -26,5 +27,39 @@ defmodule Dynomizer.ScheduleTest do
   test "changeset with invalid attributes" do
     changeset = Schedule.changeset(%Schedule{}, @invalid_attrs)
     refute changeset.valid?
+  end
+
+  # {created, updated, deleted, unchanged}
+
+  test "partition when no oldies or newbies" do
+    assert Schedule.partition([], []) == {[], [], [], []}
+  end
+
+  test "partition when no oldies" do
+    s = %Schedule{id: 1, updated_at: DateTime.utc_now()}
+    assert Schedule.partition([], [s]) == {[s], [], [], []}
+  end
+
+  test "partition when no newbies" do
+    s = %Schedule{id: 1, updated_at: DateTime.utc_now()}
+    assert Schedule.partition([s], []) == {[], [], [s], []}
+  end
+
+  test "unchanged" do
+    s = %Schedule{id: 1, updated_at: DateTime.utc_now()}
+    assert Schedule.partition([s], [s]) == {[], [], [], [s]}
+  end
+
+  test "all four" do
+    # For purposes of testing, updated_at doesn't need to be a DateTime
+    created = %Schedule{id: 1, updated_at: 0}
+    deleted = %Schedule{id: 2, updated_at: 0}
+    unchanged = %Schedule{id: 3, updated_at: 0}
+    changed_1 = %Schedule{id: 4, updated_at: 0}
+    changed_2 = %Schedule{id: 4, updated_at: 1}
+
+    oldies = [deleted, unchanged, changed_1]
+    newbies = [created, unchanged, changed_2]
+    assert Schedule.partition(oldies, newbies) == {[created], [changed_2], [deleted], [unchanged]}
   end
 end
--- web/models/schedule.ex
+++ web/models/schedule.ex
@@ -8,17 +8,58 @@ defmodule Dynomizer.Schedule do
     field :schedule, :string
     field :description, :string
     field :state, :string, size: 32
+    field :method, :string, virtual: true
 
     timestamps()
   end
 
-  # Given a schedule string return either :cron or :at.
+  @doc """
+  Given a schedule string return either :cron or :at.
+
+  ## Examples
+
+  iex> alias Dynomizer.Schedule, as: S
+  iex> S.method(%S{schedule: "3 5 * * *"})
+  :cron
+  iex> alias Dynomizer.Schedule, as: S
+  iex> S.method(%S{schedule: "2017-01-15 12:34:56"})
+  :at
+  """
   def method(schedule) do
     if schedule.schedule |> String.trim |> String.split |> length == 5 do
       :cron
     else
       :at
     end
+  end
+
+  @doc """
+  Given `oldies` and `newbies`, return a tuple of lists `{created, updated,
+  deleted, unchanged}`. Used by the `Dynamo.Scheduler` to determine what has
+  changed.
+  """
+  def partition(oldies, newbies) do
+    # Create maps from ids to Schedules.
+    mapify = fn xs -> xs |> Enum.map(&({&1.id, &1})) |> Enum.into(%{}) end
+    old_m = mapify.(oldies)
+    new_m = mapify.(newbies)
+    old_ids = Map.keys(old_m)
+    new_ids = Map.keys(new_m)
+
+    deleted_ids = old_ids -- new_ids
+    created_ids = new_ids -- old_ids
+    common_ids = MapSet.intersection(MapSet.new(old_ids), MapSet.new(new_ids))
+    {unchanged_ids, updated_ids} =
+      common_ids
+      |> Enum.partition(fn id ->
+           Map.get(old_m, id).updated_at == Map.get(new_m, id).updated_at
+         end)
+
+    take_scheds = fn (m, ids) -> m |> Map.take(ids) |> Map.values end
+    {take_scheds.(new_m, created_ids),
+     take_scheds.(new_m, updated_ids),
+     take_scheds.(old_m, deleted_ids),
+     take_scheds.(old_m, unchanged_ids)}
   end
 
   @doc """
