3c4194dc6a9a3febb6627801356e06516afde51c
Jim Menard <jim.menard@chloeandisabel.com>
2017-01-19 17:56:19 -0500

    fixed get_env in config files

--- config/dev.exs
+++ config/dev.exs
@@ -49,7 +49,7 @@ config :dynomizer,
 # Napper REST API client configuration
 config :napper,
   url: "https://api.heroku.com",
-  auth: "Bearer #{System.get_env(HEROKU_API_KEY)}",
+  auth: "Bearer #{System.get_env("HEROKU_API_KEY")}",
   accept: "application/vnd.heroku+json; version=3",
   master_prefix: "/apps"
 
